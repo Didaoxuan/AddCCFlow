6cbcf4ec244aaaf18b4345210dcc9e074cc187f5
admin <didaoxuan@outlook.com>
2023-12-10 19:44:16 +0800

    feat: 集成ccflow
    ccflow官网：http://ccflow.org/index.html

---
+++ AddCCFlow/NetCore/CcHandlerAppBuilderExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace CCFlow
+{
+    public static class CcHandlerAppBuilderExtensions
+    {
+        public static IApplicationBuilder UseCcHandler(this IApplicationBuilder app)
+        {
+            ArgumentNullException.ThrowIfNull(app);
+
+            return app.UseMiddleware<CcHandlerMiddleware>();
+        }
+    }
+}
---
+++ AddCCFlow/NetCore/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace CCFlow
+{
+    //public class Program
+    //{
+    //    public static void Main(string[] args)
+    //    {
+    //        CreateHostBuilder(args).Build().Run();
+    //    }
+
+    //    public static IHostBuilder CreateHostBuilder(string[] args) =>
+    //        Host.CreateDefaultBuilder(args)
+    //        .UseContentRoot(Directory.GetCurrentDirectory())
+    //            .ConfigureWebHostDefaults(webBuilder =>
+    //            {
+    //                webBuilder.UseStartup<Startup>();
+    //            });
+
+    //    //public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+    //    //   WebHost.CreateDefaultBuilder(args)
+    //    //       .UseStartup<Startup>()
+    //    //   .UseUrls("http://*:5000;https://*:5001");
+    //}
+}
